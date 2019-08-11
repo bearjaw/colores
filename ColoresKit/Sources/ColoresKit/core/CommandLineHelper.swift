2ca38eb30e717244a64953efcb03044fde432957
Max Baumbach <bearjaw.dev@gmail.com>
2019-08-11 11:59:22 +0200

    Check for output path argument

--- ColoresKit/Sources/ColoresKit/core/CommandLineHelper.swift
+++ ColoresKit/Sources/ColoresKit/core/CommandLineHelper.swift
@@ -19,24 +19,37 @@ public class CommandLineHelper {
     }
     
     private static func parseSketchFile(for arguments: [String], at index: Int) {
-        if validateSketch(arguments, index: index) {
+        if validateArgument(arguments, index: index) {
+            let url = parseURLIfAvailable(arguments: arguments)
             let file = arguments[index+1]
-            createColorSets(from: file)
+            createColorSets(from: file, output: url)
         } else {
             print("Please provide a path to an unzipped sketch file:")
             if let url = readLine(), !url.isEmpty {
-                createColorSets(from: url)
+                let url = parseURLIfAvailable(arguments: arguments)
+                createColorSets(from: url, output: url)
             } else {
                 print("No file path provided ¯\\_(ツ)_/¯")
             }
         }
     }
     
-    private static func createColorSets(from file: String) {
+    private static func createColorSets(from file: String, output url: URL? = nil) {
         let url = URL(fileURLWithPath: file).appendingPathComponent("document").appendingPathExtension("json")
         print("Generating colorsets")
         ColoresKit.generateColors(fromAssets: url, outputURL: url, fileType: .sketch)
     }
+    
+    private static func validateArgument(_ arguments: [String], index: Int) -> Bool {
+        return index+1 < arguments.count && !arguments[index+1].isEmpty
+    }
+    
+}
+
+
+// MARK: - Sketch
+
+extension CommandLineHelper {
     
     private static func hasSketchArgument(arguments: [String]) -> Int? {
         guard let index = arguments.firstIndex(of: "--sketch") else {
@@ -46,9 +59,28 @@ public class CommandLineHelper {
         return index
     }
     
-    private static func validateSketch(_ arguments: [String], index: Int) -> Bool {
-        return index+1 < arguments.count && !arguments[index+1].isEmpty
+}
+
+// MARK: - Output Path provided
+
+extension CommandLineHelper {
+    
+    private static func hasOuputArgument(arguments: [String]) -> Int? {
+        guard let index = arguments.firstIndex(of: "--output") else {
+            guard let index = arguments.firstIndex(of: "-o") else { return nil }
+            return index
+        }
+        return index
     }
+    
+    private static func parseURLIfAvailable(arguments: [String]) -> URL? {
+        if let outputIndex = hasOuputArgument(arguments: arguments), validateArgument(arguments, index: outputIndex) {
+            return URL(fileURLWithPath: arguments[outputIndex+1])
+        } else {
+            return nil
+        }
+    }
+    
 }
 
 
