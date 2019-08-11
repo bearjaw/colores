73e0f15fadd811f7c775ad353fb67171329fb96b
Max Baumbach <bearjaw.dev@gmail.com>
2019-08-11 12:48:49 +0200

    Use output path if provided for defined JSON files

--- ColoresKit/Sources/ColoresKit/ColoresKit.swift
+++ ColoresKit/Sources/ColoresKit/ColoresKit.swift
@@ -23,23 +23,24 @@ final public class ColoresKit: NSObject {
     }
     
     private static func createColors(from json: [String: [String]], at url: URL, output: URL? = nil, using config: ColoresConfig) {
-       let output = try! FileService.createDirectoryIfNeeded(named: "tempAssets", at: URL(fileURLWithPath: ""))
-        for (key, value) in json {
-            do {
-                guard let folder = try FileService.createDirectoryIfNeeded(named: "\(key).colorset", at: output!) else { continue }
+        do {
+            let outputURL = output != nil ? output : try FileService.createDirectoryIfNeeded(named: "tempAssets", at: URL(fileURLWithPath: ""))
+            for (key, value) in json {
+                guard let folder = try FileService.createDirectoryIfNeeded(named: "\(key).colorset", at: outputURL!) else { continue }
                 let alpha = value.count == 2 ? value.last! : "1.000"
                 let colorString = value.first!
                 let colorSet = Parser.decodeColors(from: colorString, alpha: alpha, using: config)
                 let encoded = try Parser.encodeContent(colorSet)
                 try FileService.persistFile(file: encoded, named: "Contents", fileType: .json, at: folder)
-            } catch {
-                fatalError("Error while generating color set. Error thrown \(error.localizedDescription)")
+                
             }
+        } catch {
+            fatalError("Error while generating color set. Error thrown \(error.localizedDescription)")
         }
     }
     
     private static func createColors(from sketch: SketchDocument, at url: URL, output: URL? = nil) {
-       
+        
         let sets = Parser.creeteColorSets(from: sketch, at: url)
         do {
             let outputURL = output != nil ? output : try FileService.createDirectoryIfNeeded(named: "tempAssets", at: URL(fileURLWithPath: ""))
